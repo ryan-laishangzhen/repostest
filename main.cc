6ce7cdf33b2022ef417b172a9b315a3009e2e02c
laishangzhenhen <17395928892@163.com>
2021-08-29 21:09:47 +0800

    add vscode

---
+++ main.cc
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"source.hh"
+int main()                                 //11
+{
+   // printf("add %d\n",add(2,5));            //22
+    printf("hello world\n");
+    while(1){}
+}
